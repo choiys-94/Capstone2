2fb556f4f8d1199903f261c676c1f6f9bec0ede1
choiys <hcy3bkj@gmail.com>
2018-12-06 17:35:23 +0900

    virustotal added

--- app/views.py
+++ app/views.py
@@ -12,6 +12,8 @@ import requests
 import re
 import magic
 import time
+import json
+import sys
 
 
 @app.route('/')
@@ -26,7 +28,7 @@ def about():
 def contact():
 	return render_template('contact.html')
 
-@app.route('/test')
+@app.route('/test/')
 def test_origin():
 	report_list = os.listdir('/home/choiys/cuckoo/.cuckoo/storage/analyses/')
 	report_temp = os.listdir('/home/choiys/am2pm/app/templates/reports/')
@@ -34,6 +36,10 @@ def test_origin():
 	report_list.sort()
 	report_list = report_list[0:-1]
 	report_list.sort(key=int)
+	with open("/home/choiys/am2pm/app/current", "r") as f:
+		end_num = int(f.readline())
+		count_num = int(f.readline())
+		links = json.loads(f.readline())
 	try:
 		num = report_list[-1]
 	except:
@@ -46,34 +52,45 @@ def test_origin():
 				os.symlink(src, dst)
 			except:
 				print("symlink Failed!", str(i))
-	return render_template('test.html', num = int(num))
+
+	dpath = "/home/choiys/am2pm/app/download/"
+	dnum = len(os.listdir(dpath))-1
+	dlist = os.listdir(dpath+str(dnum))
+
+	return render_template('test.html', num = int(num), end_num = end_num, count_num = count_num, links = links)
 	
 
 @app.route('/test/<passed_num>')
 def test(passed_num):
-	report_list = os.listdir('/home/choiys/cuckoo/.cuckoo/storage/analyses/')
-	report_temp = os.listdir('/home/choiys/am2pm/app/templates/reports/')
-	report_list.sort()
-	report_list = report_list[1:-1]
-	report_list.sort(key=int)
-	num = report_list[-1]
-	if int(num) > len(report_temp)-1:
-		for i in range(len(report_temp), int(num)+1):
-			src = '/home/choiys/cuckoo/.cuckoo/storage/analyses/'+str(i)+'/reports/report.html'
-			dst = '/home/choiys/am2pm/app/templates/reports/'+str(i)+'.html'
-			try:
-				os.symlink(src, dst)
-			except:
-				print("symlink Failed!", str(i))
+#	report_list = os.listdir('/home/choiys/cuckoo/.cuckoo/storage/analyses/')
+#	report_temp = os.listdir('/home/choiys/am2pm/app/templates/reports/')
+#	report_list.sort()
+#	report_list = report_list[1:-1]
+#	report_list.sort(key=int)
+	with open("/home/choiys/am2pm/app/current", "r") as f:
+		end_num = int(f.readline())
+		count_num = int(f.readline())
+#	num = report_list[-1]
+#	if int(num) > len(report_temp)-1:
+#		for i in range(len(report_temp), int(num)+1):
+#			src = '/home/choiys/cuckoo/.cuckoo/storage/analyses/'+str(i)+'/reports/report.html'
+#			dst = '/home/choiys/am2pm/app/templates/reports/'+str(i)+'.html'
+#			try:
+#				os.symlink(src, dst)
+#			except:
+#				print("symlink Failed!", str(i))
 	
 	if passed_num:
-		return render_template('test.html', num = int(num), passed_num = int(passed_num))
+		return render_template('test.html', num = int(end_num), passed_num = int(passed_num), end_num = end_num, count_num = count_num)
 	else:
-		return render_template('test.html', num = int(num))
+		return render_template('test.html', num = int(end_num), end_num = end_num, count_num = count_num)
 
-@app.route('/report/<num>')
+@app.route('/report/<num>/')
 def report(num):
+	if str(num) == 'latest':
+		return render_template('reports/latest.html')
 	return render_template('reports/'+num+'.html')
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -104,7 +121,7 @@ def search():
 		else:
 			return base_url+"/"+url
 
-	def download(s, url, file_name, temp):
+	def download(s, url, file_name, temp, links):
 		if file_name != "" and "javascript:" not in file_name and "javascript:" not in url:
 			if len(file_name)>50:
 				file_name = file_name[:50]
@@ -115,30 +132,43 @@ def search():
 				try:
 					res = s.get(url, headers=HEADER, verify=False)
 					f.write(res.content)
+					links[file_name] = url
 				except:
 					pass
-		return temp
 
-	def filter(file_name):
+		return temp, links
+
+	def filter(file_name, links):
 		file_type = m.from_file(dpath+file_name)
 		if "HTML" in file_type or "empty" in file_type:
 			os.remove(dpath+file_name)
+			del links[file_name]
 		elif "Hangul" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".hwp")
+			links[file_name] = dpath+file_name+".hwp"
 		elif "PDF" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".pdf")
+			links[file_name] = dpath+file_name+".pdf"
 		elif "PowerPoint" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".pptx")
+			links[file_name] = dpath+file_name+".pptx"
 		elif "Excel" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".xlsx")
+			links[file_name] = dpath+file_name+".xlsx"
 		elif "Word" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".docx")
+			links[file_name] = dpath+file_name+".docx"
 		elif "Zip" in file_type or "zip" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".zip")
+			links[file_name] = dpath+file_name+".zip"
 		elif "PE32" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".exe")
+			links[file_name] = dpath+file_name+".exe"
 		elif "ASCII" in file_type:
 			os.rename(dpath+file_name, dpath+file_name+".txt")
+			links[file_name] = dpath+file_name+".txt"
+	
+		return links
 
 	def split_link(type, link, base_url):
 		if type == 1:
@@ -166,25 +196,33 @@ def search():
 
 	all_atags = soup.find_all('a')
 	delete_queue = []
-
+	links = {}
 	for atag in all_atags:
 		link, file_name = split_link(2, atag, base_url)
 #		print link
-		temp = download(s, link, file_name, temp)
+		temp, links = download(s, link, file_name, temp, links)
 
 	files = os.listdir(dpath)
 	time.sleep(1)
 	for f in files:
-		filter(f)
+		links = filter(f, links)
+
+	with open("/home/choiys/am2pm/app/linkdata", "w") as fd:
+		links = json.dumps(links)
+		fd.write(links)
 
 	files = os.listdir(dpath)
 	count = len(files)
-	return render_template('index.html', download=True, count=count, num=dnum)
+	
+	return render_template('index.html', download=True, count=count, num=dnum, original = url)
 
 @app.route('/submit', methods=['POST'])
 def submit():
 	DOWNLOAD_FOLDER = "/home/choiys/am2pm/app/download/"
 	num = request.form['num']
+	with open("/home/choiys/am2pm/app/linkdata", "r") as fd:
+		links = fd.readline()
+		links = json.loads(links)
 	dpath = DOWNLOAD_FOLDER+str(num)+'/'
 	dlist = os.listdir(dpath)
 	if len(dlist) != 0:
@@ -195,7 +233,27 @@ def submit():
 		msg = msg[:-2]
 	
 		os.system(msg)
+		if len(dlist) != 1:
+			time.sleep(2)
+		else:
+			time.sleep(float(61*len(dlist)))
+	
+	num_path = "/home/choiys/am2pm/app/current"
+	all_count = len(os.listdir("/home/choiys/cuckoo/.cuckoo/storage/analyses"))-1
 
-		time.sleep(60)
+	vt_scan_url = "https://www.virustotal.com/vtapi/v2/file/scan"
+	vt_report_url = "https://www.virustotal.com/vtapi/v2/file/report"
+	params = {'apikey': 'dcf9d3c8969c08565f0c66e80aa78cf6694d7eb249b6711fdd75ecb983ec2307'}
+	for f in dlist:
+		files = {'file': (f, open(dpath+f, 'rb'))}
+		response = requests.post(vt_scan_url, files=files, params=params)
+		params = {'apikey': 'dcf9d3c8969c08565f0c66e80aa78cf6694d7eb249b6711fdd75ecb983ec2307', 'resource': response.json()['resource']}
+		response = requests.get(vt_report_url, params=params)
+		print response.json()
+
+	with open(num_path, "w") as f:
+		f.write(str(all_count)+'\n')
+		f.write(str(len(dlist))+'\n')
+		f.write(json.dumps(links))
 
 	return render_template('index.html', submit=True)
